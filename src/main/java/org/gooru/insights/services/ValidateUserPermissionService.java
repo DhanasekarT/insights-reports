412c8142f085dfd761434f064d251c1ecbc8f4fc
daniel <arokiadaniel.a@goorulearning.org>
2014-12-19 20:03:33 +0530

    New services added for user permission validation

--- src/main/java/org/gooru/insights/services/ValidateUserPermissionService.java
+++ src/main/java/org/gooru/insights/services/ValidateUserPermissionService.java
@@ -7,23 +7,23 @@ import java.util.Set;
 import org.gooru.insights.models.RequestParamsDTO;
 import org.gooru.insights.models.RequestParamsFilterDetailDTO;
 
-public interface ValidateUserPermissionService {
+ interface ValidateUserPermissionService {
 
-	public Map<String, Object> getAllowedFilters(String gooruUId);
+	 Map<String, Object> getAllowedFilters(String gooruUId);
 	
-	public Map<String, Object> getUserFiltersAndValues(List<RequestParamsFilterDetailDTO> filters);
+	 Map<String, Object> getUserFiltersAndValues(List<RequestParamsFilterDetailDTO> filters);
 	
-	public Map<Integer, String> checkIfFieldValueMatch(Map<String, Object> allowedFilters, Map<String, Object> userFilters, Map<Integer, String> errorMap);
+	 Map<Integer, String> checkIfFieldValueMatch(Map<String, Object> allowedFilters, Map<String, Object> userFilters, Map<Integer, String> errorMap);
 	
-	public RequestParamsDTO userPreValidation(RequestParamsDTO requestParamsDTO, Set<String> userFilterUserValues, Map<String, Set<String>> partyPermissions, Map<Integer, String> errorMap);
+	 RequestParamsDTO userPreValidation(RequestParamsDTO requestParamsDTO, Set<String> userFilterUserValues, Map<String, Set<String>> partyPermissions, Map<Integer, String> errorMap);
 	
-	public RequestParamsDTO validateOrganization(RequestParamsDTO requestParamsDTO, Map<String, Set<String>> partyPermissions, Map<Integer, String> errorMap, Set<String> userFilterOrgValues);
+	 RequestParamsDTO validateOrganization(RequestParamsDTO requestParamsDTO, Map<String, Set<String>> partyPermissions, Map<Integer, String> errorMap, Set<String> userFilterOrgValues);
 	
-	public List<RequestParamsFilterDetailDTO> addSystemContentUserOrgFilter(List<RequestParamsFilterDetailDTO> userFilter, String userOrgUId);
+	 List<RequestParamsFilterDetailDTO> addSystemContentUserOrgFilter(List<RequestParamsFilterDetailDTO> userFilter, String userOrgUId);
 	
-	public List<RequestParamsFilterDetailDTO> addSystemContentOrgFilter(List<RequestParamsFilterDetailDTO> userFilter, String userOrgUId);
+	 List<RequestParamsFilterDetailDTO> addSystemContentOrgFilter(List<RequestParamsFilterDetailDTO> userFilter, String userOrgUId);
 	
-	public List<RequestParamsFilterDetailDTO> addSystemUserOrgFilter(List<RequestParamsFilterDetailDTO> userFilter, String userOrgUId);
+	 List<RequestParamsFilterDetailDTO> addSystemUserOrgFilter(List<RequestParamsFilterDetailDTO> userFilter, String userOrgUId);
 	
-	public String getAllowedParties(RequestParamsDTO requestParamsDTO,Map<String, Set<String>> partyPermissions);
+	 String getAllowedParties(RequestParamsDTO requestParamsDTO,Map<String, Set<String>> partyPermissions);
 }
