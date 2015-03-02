87637bec8f7b492566ab8ed3d915478b70245d8f
daniel <arokiadaniel.a@goorulearning.org>
2015-03-02 14:18:57 +0530

    Mail service support

--- src/main/java/org/gooru/insights/services/MailServiceImpl.java
+++ src/main/java/org/gooru/insights/services/MailServiceImpl.java
@@ -8,7 +8,9 @@ import org.springframework.mail.MailParseException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MailServiceImpl implements MailService {
 
 	@Autowired
