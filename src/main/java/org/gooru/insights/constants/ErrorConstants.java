88d5b39203762a4e098a36534cf34d27f827768e
Renuka <renuka@goorulearning.org>
2015-05-08 18:29:06 +0530

    Added error handling for invalid user inputs

--- src/main/java/org/gooru/insights/constants/ErrorConstants.java
+++ src/main/java/org/gooru/insights/constants/ErrorConstants.java
@@ -26,6 +26,9 @@ public class ErrorConstants {
 	public static final String E109 = "E109";
 	
 	public static final String E110 = "E110";
+	
+	public static final String E111 = "E111";
+
 	/**
 	 * Error fields
 	 */
--- src/main/java/org/gooru/insights/services/BaseAPIServiceImpl.java
+++ src/main/java/org/gooru/insights/services/BaseAPIServiceImpl.java
@@ -25,6 +25,7 @@ import org.gooru.insights.constants.APIConstants;
 import org.gooru.insights.constants.APIConstants.Hasdatas;
 import org.gooru.insights.constants.ErrorConstants;
 import org.gooru.insights.exception.handlers.BadRequestException;
+import org.gooru.insights.exception.handlers.ReportGenerationException;
 import org.gooru.insights.models.RequestParamsCoreDTO;
 import org.gooru.insights.models.RequestParamsDTO;
 import org.gooru.insights.models.RequestParamsFilterDetailDTO;
@@ -393,7 +394,7 @@ public class BaseAPIServiceImpl implements BaseAPIService {
 		 * Aggregation mandatory fields need to be present and it's field name should be in database acceptable field
 		 */
 		if(checkNull(requestParamsDTO.getAggregations())){
-			for(Map<String,String> aggregate : requestParamsDTO.getAggregations()){
+			for(Map<String, String> aggregate : requestParamsDTO.getAggregations()){
 				if(!aggregate.containsKey(APIConstants.FormulaFields.REQUEST_VALUES.getField()) || !aggregate.containsKey(APIConstants.FormulaFields.NAME.getField()) || !checkNull(aggregate.get(APIConstants.FormulaFields.NAME.getField())) || !aggregate.containsKey(APIConstants.FormulaFields.FORMULA.getField()) || !checkNull(aggregate.get(APIConstants.FormulaFields.FORMULA.getField())) || !aggregate.containsKey(aggregate.get(APIConstants.FormulaFields.REQUEST_VALUES.getField())) || !checkNull(aggregate.get(aggregate.get(APIConstants.FormulaFields.REQUEST_VALUES.getField())))){
 					throw new BadRequestException(MessageHandler.getMessage(ErrorConstants.E100,APIConstants.AGGREGATE_ATTRIBUTE));
 				}else{
@@ -402,6 +403,18 @@ public class BaseAPIServiceImpl implements BaseAPIService {
 					}
 					if(!baseConnectionService.getFormulaOperations().contains(aggregate.get(APIConstants.FormulaFields.FORMULA.getField()).toUpperCase())){
 						throw new BadRequestException(MessageHandler.getMessage(ErrorConstants.E103,new String[]{APIConstants.AGGREGATE_ATTRIBUTE,aggregate.get(APIConstants.FormulaFields.FORMULA.getField())}));
+					}
+					if(aggregate.containsKey(APIConstants.AggregateFields.PERCENTS.getField()) && StringUtils.isNotBlank(aggregate.get(APIConstants.AggregateFields.PERCENTS.getField()).toString())) {
+						String[] percentsArray = aggregate.get(APIConstants.AggregateFields.PERCENTS.getField()).toString().split(",");
+						for (int index = 0; index < percentsArray.length; index++) {
+							if (StringUtils.isNotBlank(percentsArray[index])) {
+								try {
+									Double.parseDouble(percentsArray[index]);
+								} catch(NumberFormatException nfe) {
+									throw new BadRequestException(MessageHandler.getMessage(ErrorConstants.E111,new String[]{APIConstants.AGGREGATE_ATTRIBUTE,aggregate.get(APIConstants.AggregateFields.PERCENTS.getField()),"double"}));
+								}
+							}
+						}
 					}
 					fieldData.add(aggregate.get(APIConstants.FormulaFields.NAME.getField()));
 				}
