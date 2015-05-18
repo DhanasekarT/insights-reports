a0cada0c3642a69a67c22356610505b520593bdf
manikandan <manikandan.s@bambeeq.com>
2015-05-18 18:52:54 +0530

    Fixed issue on scheduler

--- src/main/java/org/gooru/insights/scheduler/SchedulerJob.java
+++ src/main/java/org/gooru/insights/scheduler/SchedulerJob.java
@@ -1,18 +1,18 @@
 package org.gooru.insights.scheduler;
 
-import org.gooru.insights.services.ItemService;
+import org.gooru.insights.services.CSVFileWriterService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class SchedulerJob {
 
 	@Autowired
-	private ItemService itemService;
+	private CSVFileWriterService csvWriterService;
 	
-	public ItemService getItemService() {
-		return itemService;
+	public CSVFileWriterService getCSVFileWriterService() {
+		return csvWriterService;
 	}
 	
 	public void removeExpiredFiles() {
-		getItemService().removeExpiredFile();
+		getCSVFileWriterService().removeExpiredFile();
 	}
 }
